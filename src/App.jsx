e1f6f18129ca95b80be513cf3df421e36d0131f1
tasfia06 <tasfiaj027@gmail.com>
2026-07-27 13:03:45 -0700

    Fix Yosemite webpage bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 function App() {
   return (
     <div className="container">
-    <h1 className="title">Yosemite National Park</h1>
-    <img className="scenery-img-styled" src="./scenery.jpg" alt="A beautiful scenery" />
+      <h1 className="title">Yosemite National Park</h1>
+
+      <img
+        className="scenery-img"
+        src="/scenery.jpeg"
+        alt="Half Dome in Yosemite National Park"
+      />
+
       <p className="description">
         A picture of Yosemite National Park's Half Dome, halfway through the 4 Mile Trail.
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
